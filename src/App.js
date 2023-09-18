1a5dd2005101adf64e7c1dc97f54fbc4c5d8f079
kithefedeshepe <103298577+kithefedeshepe@users.noreply.github.com>
2023-09-18 15:42:07 +0800

    structure doc v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Login, homepage
+import HomePage from "./HomePage/HomePage";
+import LoginPage from "./logPage/LoginPage";
+
+//DoctorMainPage
+import DoctorMainPage from "./DoctorMainPage/DoctorMainPage";
+
+//DoctorUploadPage
+import DoctorUploadPage from "./DoctorUploadPage/DoctorUploadPage";
+
+//DoctorViewResult
+import DoctorViewResult from "./DoctorViewResult/DoctorViewResult";
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+			<Routes>
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/" element={<HomePage />} />
+
+        //doctor
+				<Route path="/DoctorMainPage" element={<DoctorMainPage />} />
+				<Route path="/DoctorUploadImage/doc-upload-image" element={<DoctorUploadPage />} />
+				<Route path="/UserAdminPage/doc-view-result" element={<DoctorViewResult />} />
+
+			</Routes>
+		</Router>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +45,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
