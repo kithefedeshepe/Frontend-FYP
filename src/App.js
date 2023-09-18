f4fea68fb7819b09bf6d1dfb85d03bd83f1f56ce
kithefedeshepe <103298577+kithefedeshepe@users.noreply.github.com>
2023-09-18 15:15:17 +0800

    doctor folders

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> TESTINGGGG 1
+          Edit <code>src/App.js</code> new folders added
         </p>
         <a
           className="App-link"
